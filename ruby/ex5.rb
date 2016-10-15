a48367b54cdf921d344e30664dec41bc27062bdd
Adam Dipinto <adam.dipinto@gmail.com>
2016-10-15 19:01:20 +0000

    Adding exercises 5 and 6

---
+++ ruby/ex5.rb
@@ -0,0 +1,23 @@
+#More Variables and Printing
+
+name = 'Zed A. Shaw'
+age = 35 # not a lie in 2009
+height = 74 # inches
+weight = 180 # lbs
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+
+#inches_to_centimeters = height * 2.54
+#pounds_to_kilograms = weight * 0.454
+
+puts "Let's talk about #{name}."
+puts "He's #{height} inches tall."
+puts "He's #{weight} pounds heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His teeth are usaully #{teeth} depending on the coffee."
+
+# This line is tricky, try to get it exactly right
+puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}."
