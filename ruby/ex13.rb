51581400db572c7e4e3cef75b098301565be191d
Adam Dipinto <adam.dipinto@gmail.com>
2016-10-15 23:10:06 +0000

    Adding exercises 13 and 14

---
+++ ruby/ex13.rb
@@ -0,0 +1,11 @@
+#Parameters, Unpacking, Variables
+
+
+#Need to input the variables in the command line when calling ex13
+#ex: ruby ex13.rb first 2nd third
+
+first, second, third = ARGV
+
+puts "Your first variable is: #{first}"
+puts "Your second variable is: #{second}"
+puts "Your third variable is #{third}"
