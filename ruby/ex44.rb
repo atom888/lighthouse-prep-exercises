f16712a34f6265192a031799c36e6f48ab157e58
Adam Dipinto <adam.dipinto@gmail.com>
2016-10-25 21:52:16 +0000

    Adding exercise 44

---
+++ ruby/ex44.rb
@@ -0,0 +1,95 @@
+# Inheritance vs. Composition
+
+
+## Inheritance
+class Parent
+
+  def override()
+    puts "PARENT override()"
+  end
+
+  def implicit()
+    puts "PARENT implicit()"
+  end
+
+  def altered()
+    puts "PARENT altered()"
+  end
+end
+
+class Child < Parent
+
+  def override()
+    puts "CHILD override()"
+  end
+
+  def altered()
+    puts "CHILD, BEFORE PARENT altered()"
+    ## Using super() to get the Parent.altered version - function runs
+    ## and prints out the Parent message
+    super()
+    puts "CHILD, AFTER PARENT altered()"
+  end
+
+end
+
+dad = Parent.new()
+son = Child.new()
+
+dad.implicit()
+son.implicit()
+
+dad.override()
+son.override()
+
+dad.altered()
+son.altered()
+
+
+##########################################################
+
+## Composition
+
+class Other
+
+  def override()
+    puts "OTHER override()"
+  end
+
+  def implicit()
+    puts "OTHER implicit()"
+  end
+
+  def altered()
+    puts "OTHER altered()"
+  end
+end
+
+class Child
+
+  def initialize()
+    @other = Other.new()
+  end
+
+  def implicit()
+    @other.implicit()
+  end
+
+  def override()
+    puts "CHILD override()"
+  end
+
+  def altered()
+    puts "CHILD, BEFORE OTHER altered()"
+    @other.altered()
+    puts "CHILD, AFTER OTHER altered()"
+  end
+end
+
+
+son = Child.new()
+
+son.implicit()
+son.override()
+son.altered()
+
